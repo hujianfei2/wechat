1707585cd2ef7093ecb9c4826ace0bf1e0df491c
wuxinyu_ext <hujf3@asiainfo.com>
2024-02-06 02:09:08 +0800

    author:hujf commit date:2024-02-06

---
+++ wechat-chat-server/src/main/java/com/hujf/wechat/common/WeChatCustomerApplication.java
@@ -0,0 +1,15 @@
+package com.hujf.wechat.common;
+
+/**
+ * @author hujf
+ * @date 2024/02/06
+ */
+@SpringBootApplication(scanBasePackages = {"com.hujf.wechat"})
+public class WeChatCustomerApplication {
+
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+            SpringApplication.run(MallchatCustomApplication.class,args);
+        }
+    }
+}
